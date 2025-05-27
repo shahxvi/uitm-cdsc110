f6d2b5364b07ddf3b80b0f0d0849090d68d48637
Shah <irfanshahxvi@gmail.com>
2025-05-27 08:14:27 +0800

    Added Answer

--- CSC126/W8 Repetition Lab Module 2/08. Lesson 3, Question 2 (c).cpp
+++ CSC126/W8 Repetition Lab Module 2/08. Lesson 3, Question 2 (c).cpp
@@ -4,17 +4,16 @@ using namespace std;
 int main () {
     
     int i, j;
-    char at = '@';
 
     for (i = 0; i < 5; i++) {
 
         for (j = 0; j < 5; j++) {
-            cout << at;
+            cout << "@";
             cout << endl;
         }
 
         for (j = 0; j < 1; j++)
-            cout << at;
+            cout << "@";
     }
 
 
