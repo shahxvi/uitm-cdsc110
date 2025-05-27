2e2d78931d5b2fd1f3b8f12d370a57ca04af557d
Shah <irfanshahxvi@gmail.com>
2025-05-27 10:54:30 +0800

    Update Code

--- CSC126/W8 Repetition Lab Module 2/05. Lesson 3, Question 1.cpp
+++ CSC126/W8 Repetition Lab Module 2/05. Lesson 3, Question 1.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
+#include <string.h>
 using namespace std;
 
 int main() {
 	
-    char name[30];
+    char name[30], smartestStudent[30];
     float mark = 0, total, average;
+    float highestMark = 0, lowestMark = 100, highestAverage = 0,failedCount = 0;
 
     for (int students = 0; students < 3; students++) {      //outerLoop for students
     	
@@ -14,18 +16,45 @@ int main() {
 
         total = 0; //Reset total mark for each student
 
-        for (int subject = 1; subject < 5; subject++) { //innerLoop for subjects
+        for (int subject = 1; subject < 5; subject++) {     //innerLoop for subjects
         	
             cout << "Enter the mark for subject " << (subject) << ": ";
             cin >> mark;
             total += mark;
+
+            if (mark > highestMark) {
+                highestMark = mark;
+            }
+
+            if (mark < lowestMark) {
+                lowestMark = mark;
+            }
         }
 
         average = total / 4.0;
-
-        cout << "\nThe average mark for " << name << " is " << average << endl;
+    
+        if (average > highestAverage) {
+            highestAverage = average;
+            strcpy(smartestStudent, name); // Store the name of the smartest student
+        }
+        
+        if (average < 50) {
+            failedCount++; // Count the number of students who failed
+        }
+        
+        cout << "\nThe highest mark for " << name << " is: " << highestMark << endl;
+        cout << "The lowest mark for " << name << " is: " << lowestMark << endl;
     }
-    cout << endl;
-    system("pause");
-    return 0;
+
+    //1 - find and disply the lowest and highest mark for each student
+    //line 41 & 42
+
+    //2 - find and display the name of the smartest student (highest average)
+    // Note: This requires storing the highest average and corresponding student name.
+    cout << "\nThe smartest student is " << smartestStudent << "who has an average of " << highestAverage << endl;
+
+    //3 - count & display the number of that failed (average < 50)
+    // Note: This requires checking the average for each student.
+    cout << "\nThe number of students who failed is: " << failedCount << endl;
+
 }
