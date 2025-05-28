79981205c1b941fdbbd86ff8fe1ab0ff018d49e1
Shah <irfanshahxvi@gmail.com>
2025-05-28 21:16:18 +0800

    Update Code

--- CSC126/W9 Repetition Class Exercise 2/05. Question 4.cpp
+++ CSC126/W9 Repetition Class Exercise 2/05. Question 4.cpp
@@ -14,44 +14,39 @@ int main() {
         cout << "\nFrom 1 to 5 (least to most likely) of getting a high marks for any programming subject: ";
         cin >> score;
 
-        switch (score) {
-            case 1:
-                score1++;
-                break;
-            case 2:
-                score2++;
-                break;
-            case 3:
-                score3++;
-                break;
-            case 4:
-                score4++;
-                break;
-            case 5:
-                score5++;
-                break;
-            default:
-                cout << "Invalid score. Please enter a score between 0 and 5." << endl;
-                i = 1;
-                invalidScore++;
-                break;
-        }
-
-
-        if (score >= 1 || score <= 5) {
+        if (score >= 1 && score <= 5) {
+            switch (score) {
+                case 1:
+                    score1++;
+                    break;
+                case 2:
+                    score2++;
+                    break;
+                case 3:
+                    score3++;
+                    break;
+                case 4:
+                    score4++;
+                    break;
+                case 5:
+                    score5++;
+                    break;
+            }
+            
             validScore++;
             correspondentsValid++;
+            correspondent++;
+
+            cout << "Continue for another correspondent? (1 for Yes, 0 for No): ";
+            cin >> i;
+        
         }
 
         else {
+            cout << "Invalid score. Please enter a score between 1 and 5." << endl;
             invalidScore++;
             correspondentsInvalid++;
         }
-
-        correspondent++;
-
-        cout << "Continue for another correspondent? (1 for Yes, 0 for No): ";
-        cin >> i;
     } 
     
     cout << "Number of respondents that answered 1: " << score1 << endl;
@@ -62,4 +57,8 @@ int main() {
 
     cout << "\nTotal correspondents: " << correspondent << endl;
     cout << "Average score result: " << (score1 + score2 + score3 + score4 + score5) / correspondent << endl;
+
+    cout << endl;
+    system("pause");
+    return 0;
 }
